006f9af72318c8105b815263d861a966a8899777
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-10-26 20:38:00 -0500

    removed all hashes

---
+++ conditional1.rb
@@ -0,0 +1,99 @@
+# 1
+# example = 10
+# if example == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 2
+# i = 15
+# if i < 10
+#   p -1
+# elsif i == 10
+#   p 0
+# else
+#   p 1
+# end
+
+# 3
+# a = 10
+# b = 10
+# if a < 10 && b < 10
+#   p 1
+# else
+#   p 0
+# end
+
+# 4
+# c = 54
+# if c > 9000
+#   p 1
+# else 
+#   p -1
+# end
+
+# 5
+# a = 38
+# if a < 10
+#   p 9
+# elsif a < 20
+#   p 19
+# elsif a < 30
+#   p 29
+# else
+#   p -1
+# end
+
+# 6
+# a = 6
+# b = 5
+# if a > 10 || b > 10
+#   p 100
+# else
+#   p -100
+# end
+
+# 7
+# a = 1
+# if a < 0
+  p 1776
+elsif a > 0
+  p 1976
+end
+
+8
+a = 9
+if a == 100
+  p 100
+elsif a == 99
+  p 99
+else
+  p 0
+end
+
+9
+a = -1
+b = 1
+if a < 0 && b > 0
+  p 1
+else
+  p 0
+end
+
+10
+a = 15
+if a > 80
+  p 5
+elsif a > 60
+  p 4
+elsif a > 40
+  p 3
+elsif a > 20
+  p 2
+else
+  p 1
+end
+
+
+
